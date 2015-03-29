2013a88dc3001327b5fe4692867c7164e4473062
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-29 10:27:20 -0700

    moved paste wrapper helper fxn to zzz, cleaned out unused code

--- R/src_couchdb.R
+++ R/src_couchdb.R
@@ -14,31 +14,11 @@ src_couchdb <- function(type = "localhost", port = 5984, user = NULL, pwd = NULL
   cush <- sofa::cushions()[[type]]
   info <- sofa::ping()
   dbs <- sofa::db_list()
-#   defdb <- defaultdb(db)
-#   dbinfo <- db_info(dbname = defdb)
-#   if(!is.null(dbinfo$error)) db_create(dbname=defdb)
-#   dbout <- sofa::db_info(dbname = defdb)
   structure(c(info, cush), class=c("src_couchdb","docdb_src"), type="couchdb", dbs=dbs)
 }
 
 #' @export
 print.src_couchdb <- function(x, ...){
   cat(sprintf("src: %s %s [%s/%s]", attr(x, "type"), x$version, x$type, x$port), sep = "\n")
-#   cat(sprintf("db: %s", attr(x, "dbname")), sep = "\n")
   cat(doc_wrap("databases: ", paste0(attr(x, "dbs"), collapse = ", "), width=80), "\n", sep = "")
-}
-
-doc_wrap <- function (..., indent = 0, width = getOption("width")){
-  x <- paste0(..., collapse = "")
-  wrapped <- strwrap(x, indent = indent, exdent = indent + 5, width = width)
-  paste0(wrapped, collapse = "\n")
-}
-
-couchdb_default <- "docdbi"
-defaultdb <- function(x = NULL) {
-  if(is.null(x) || !is.character(x)) {
-    couchdb_default
-  } else {
-    x
-  }
 }
--- R/zzz.R
+++ R/zzz.R
@@ -5,3 +5,9 @@ pluck <- function(x, name, type) {
     vapply(x, "[[", name, FUN.VALUE = type)
   }
 }
+
+doc_wrap <- function (..., indent = 0, width = getOption("width")){
+  x <- paste0(..., collapse = "")
+  wrapped <- strwrap(x, indent = indent, exdent = indent + 5, width = width)
+  paste0(wrapped, collapse = "\n")
+}
