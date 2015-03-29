cd2556fb44b527fffadb147a578077d2b5f4d327
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-29 08:24:06 -0700

    added couchdb version of delete

--- R/delete.R
+++ R/delete.R
@@ -5,6 +5,12 @@
 #' @param key A key
 #' @param ... Ignored for now
 #' @examples \dontrun{
+#' # couchdb
+#' src <- src_couchdb()
+#' docdb_create(src, "mtcars", mtcars)
+#' docdb_get(src, "mtcars")
+#' docdb_delete(src, "mtcars")
+#'
 #' # etcd
 #' src <- src_etcd()
 #' docdb_create(src, "/iris", iris)
@@ -15,10 +21,10 @@ docdb_delete <- function(src, key, ...){
   UseMethod("docdb_delete")
 }
 
-# #' @export
-# docdb_delete.src_couchdb <- function(src, key, ...){
-#   dropmeta(rbindlist(pluck(sofa::alldocs(cushion = src$type, dbname = docid, include_docs = TRUE)$rows, "doc")))
-# }
+#' @export
+docdb_delete.src_couchdb <- function(src, key, ...){
+  sofa::db_delete(cushion = src$type, dbname = key)
+}
 
 #' @export
 docdb_delete.src_etcd <- function(src, key, ...){
