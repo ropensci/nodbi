1a937f69a9895b550ab28ee434540c88e2e0c57f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-31 18:01:31 -0700

    updated pkg level man file with authors

--- R/docdbi-package.R
+++ R/docdbi-package.R
@@ -3,5 +3,8 @@
 #' @name docdbi-package
 #' @aliases docdbi
 #' @docType package
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @author Rich FitzJohn \email{rich.fitzjohn@@gmail.com}
+#' @author Jeroen Ooms \email{jeroen.ooms@@stat.ucla.edu}
 #' @keywords package
 NULL
