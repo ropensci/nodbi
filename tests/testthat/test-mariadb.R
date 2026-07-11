ca6fd5901b3520daeda083768ec16638d9346846
Ralf Herold <ralf.herold@gmx.net>
2026-07-11 20:36:32 +0200

    add testing for mariadb

---
+++ tests/testthat/test-mariadb.R
@@ -0,0 +1,24 @@
+test_that("Source", {
+  skip_if_no_mariadb()
+  skip_on_cran()
+
+  # creating database connection has
+  # unique parameters, see README.Rmd
+  dbSrcKey <- function() {
+    testKey <- createKey()
+    testSrc <- src_mariadb()
+    return(list(testKey = testKey, testSrc = testSrc))
+  }
+
+  # test database set up
+  tmp <- dbSrcKey()
+  expect_is(tmp$testSrc, "docdb_src")
+  expect_is(tmp$testSrc, "src_mariadb")
+  expect_output(suppressWarnings(print(tmp$testSrc)), "src: MariaDB")
+  RMariaDB::dbDisconnect(tmp$testSrc$con)
+  rm(tmp)
+
+  # run generic (database independent) tests
+  # source("core-nodbi.R", local = TRUE)
+
+})
